9c4e985203048470d0096461b1ab6898c53cf4c0
parisa <parisahazhirghader@gmail.com>
2021-08-27 18:26:18 +0430

    init commit

---
+++ app/src/main/java/com/fluttycatgeek/mvi_cleanarchitecture/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fluttycatgeek.mvi_cleanarchitecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
