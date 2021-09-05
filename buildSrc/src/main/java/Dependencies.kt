63b5a8b9e37e233559d3ed7498469d5e6defd20b
fluttycatgeek <fluttycatgeek@gmail.com>
2021-09-05 15:56:37 +0430

    dependencies added.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,23 +1,92 @@
-
 /**
  * To define plugins
  */
 object BuildPlugins {
-    val androidGradlePlugin by lazy { "com.android.tools.build:gradle:${Versions.gradlePlugin}" }
-    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}" }
+    const val androidGradle = "com.android.tools.build:gradle:${Versions.gradlePlugin}"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
 }
 
 /**
  * To define dependencies
  */
-object Deps {
-    val appCompat by lazy { "androidx.appcompat:appcompat:${Versions.appCompat}" }
-    val timber by lazy { "com.jakewharton.timber:timber:${Versions.timber}" }
-    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}" }
-    val materialDesign by lazy { "com.google.android.material:material:${Versions.material}" }
-    val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}" }
-    val junit by lazy { "junit:junit:${Versions.jUnit}" }
+object Libs {
 
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:_"
+    const val kotlinCoroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:_"
+    const val kotlinCoroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:_"
+    const val kotlinCoroutinesDebug = "org.jetbrains.kotlinx:kotlinx-coroutines-debug:_"
+    const val coroutineLifecycleHandler = "com.github.hadilq:coroutinelifecyclehandler:_"
+
+    const val appCompat = "com.android.support:appcompat-v7:_"
+    const val design = "com.android.support:design:_"
+    const val supportAnnotations = "com.android.support:support-annotations:_"
+    const val cardView = "com.android.support:cardview-v7:_"
+    const val recyclerview = "androidx.recyclerview:recyclerview:_"
+    const val material = "com.google.android.material:material:_"
+    const val constraintLayout = "com.android.support.constraint:constraint-layout:_"
+    const val timber = "com.jakewharton.timber:timber:_"
+    const val dagger = "com.google.dagger:dagger:_"
+    const val daggerCompiler = "com.google.dagger:dagger-compiler:_"
+    const val daggerAndroid = "com.google.dagger:dagger-android:_"
+    const val daggerAndroidSupport = "com.google.dagger:dagger-android-support:_"
+    const val daggerAndroidProcessor = "com.google.dagger:dagger-android-processor:_"
+
+    const val androidLegacySupportV4 = "androidx.legacy:legacy-support-v4:_"
+
+    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:_"
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:_"
+
+    const val lifecycle = "androidx.lifecycle:lifecycle-extensions:_"
+    const val lifecycleCompiler = "androidx.lifecycle:compiler:_"
+    const val lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:_"
+    const val lifecycleLiveData = "androidx.lifecycle:lifecycle-livedata-ktx:_"
+
+    const val retrofit = "com.squareup.retrofit2:retrofit:_"
+    const val retrofitGson = "com.squareup.retrofit2:converter-gson:_"
+    const val retrofitCoroutinesAdapter =
+        "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:_"
+
+    const val okhttp3Logging = "com.squareup.okhttp3:logging-interceptor:_"
+
+    const val fileChooser = "com.github.hedzr:android-file-chooser:_"
+    const val picasso = "com.squareup.picasso:picasso:_"
+    const val picassoTransformations = "jp.wasabeef:picasso-transformations:_"
+
+    const val imageCropper = "com.theartofdev.edmodo:android-image-cropper:_"
+
+    const val materialishProgress = "com.pnikosis:materialish-progress:_"
+    const val circleProgressView = "com.github.jakob-grabner:Circle-Progress-View:_"
+
+    const val firebaseMessaging = "com.google.firebase:firebase-messaging:_"
+    const val firebaseAnalytics = "com.google.firebase:firebase-analytics:_"
+    const val crashlytics = "com.crashlytics.sdk.android:crashlytics:_"
+    const val googleAuth = "com.google.android.gms:play-services-auth:_"
+    const val googleLocation = "com.google.android.gms:play-services-location:_"
+    const val googleServicesBase = "com.google.android.gms:play-services-base:17.2.1"
+
+    const val discreteScrollview = "com.yarolegovich:discrete-scrollview:_"
+    const val flexbox = "com.google.android:flexbox:_"
+    const val circularImageview = "com.mikhaellopez:circularimageview:_"
+    const val dexter = "com.karumi:dexter:_"
+    const val jsoup = "org.jsoup:jsoup:_"
+    const val multidex = "com.android.support:multidex:_"
+
+
+}
+
+object TestLibs {
+    const val testRunner = "androidx.test:runner:_"
+    const val testRules = "androidx.test:rules:_"
+    const val testExtJunit = "androidx.test.ext:junit:_"
+    const val testExtTruth = "androidx.test.ext:truth:_"
+    const val testGoogleTruth = "com.google.truth:truth:_"
+    const val espressoCore = "androidx.test.espresso:espresso-core:_"
+
+    const val jUnit = "junit:junit:_"
+    const val mockitoCore = "org.mockito:mockito-core:_"
+    const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:_"
+    const val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:_"
+    const val androidArchCoreTesting = "android.arch.core:core-testing:_"
 }
 
 
