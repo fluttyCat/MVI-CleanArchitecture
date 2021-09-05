955b0f872d798718432ce94cc64e9f3afcc64b81
fluttycatgeek <fluttycatgeek@gmail.com>
2021-09-05 20:26:22 +0430

    kotlin version added.

---
+++ buildSrc/src/main/java/BuildPlugins.kt
@@ -0,0 +1,4 @@
+object BuildPlugins {
+    const val androidGradle = "com.android.tools.build:gradle:${Versions.gradlePlugin}"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+}
