1cb8746e8ec6d5bc641f6678030858da2d489827
fluttycatgeek <fluttycatgeek@gmail.com>
2021-09-05 22:41:26 +0430

    build.gradle(:app) : adding dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,6 +3,7 @@ object Versions {
     const val gradlePlugin = "4.2.2"
     const val kotlin = "1.5.30"
     const val timber = "4.7.1"
+    const val core = "1.6.0"
     const val appCompat = "1.3.1"
     const val material = "1.4.0"
     const val constraintLayout = "2.1.0"
