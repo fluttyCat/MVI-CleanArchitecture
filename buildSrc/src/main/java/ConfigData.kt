c0ab228cb4cb754bcfdef84b3a40525fd4994b4b
fluttycatgeek <fluttycatgeek@gmail.com>
2021-09-05 15:47:10 +0430

    build.gradle.kts, buildSrc.jar added.

---
+++ buildSrc/src/main/java/ConfigData.kt
@@ -0,0 +1,12 @@
+object ConfigData {
+
+    const val compileSdkVersion = 31
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 31
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val applicationId = "com.fluttycatgeek.mvi_cleanarchitecture"
+    const val DEBUG = "debug"
+    const val RELEASE = "release"
+}
